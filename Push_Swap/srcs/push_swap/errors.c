f3f239c65c8e97a7ff388a480890caea68183df4
JulesPelletier <julespelletier.perso@gmail.com>
2021-07-29 08:30:55 +0200

    test

---
+++ Push_Swap/srcs/push_swap/errors.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Jules <Jules@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 16:04:54 by Jules             #+#    #+#             */
+/*   Updated: 2021/07/27 18:46:11 by Jules            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	ft_error2(char *str)
+{
+	if (str[0] == 'e')
+		return ;
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
+void	ft_error(char *str, t_push_swap *all)
+{
+	if (all->flag[4])
+		freechar(all->av_dup);
+	free(all->str);
+	if (str[0] == '1')
+		ft_error2("malloc");
+	free(all->stA.st);
+	if (str[0] == '2')
+		ft_error2("malloc");
+	free(all->stB.st);
+	ft_error2("malloc");
+}
---
+++ Push_Swap/srcs/push_swap/push_swap.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Jules <Jules@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 15:46:14 by Jules             #+#    #+#             */
+/*   Updated: 2021/07/28 08:56:59 by Jules            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void    algo_check(t_push_swap *all)
+{
+	init_stack(&all->stA);
+	if (all->stA.len == 2)
+		update_output(all, "sa");
+	else if (all->stA.len == 3)
+		algo_three(all);
+	else if (all->stA.len <= 15)
+		algo_quinze(all);
+	else
+		algo_quinze_plus(all);
+}
+
+int		check_flag(char **av)
+{
+	if (av[1][0] == '-' && ft_isalpha(av[1][1]) == 1)
+		return (-1);
+	else
+		return (1);
+}
+
+int	main(int ac, char **av)
+{
+	t_push_swap	all;
+
+	all.ac_dup = ac;
+	if (all.ac_dup >= 2)
+	{
+		all.av_dup = all_char(ac, av, &all);
+		all_init(all.av_dup, &all);
+		if (!all.stA.st || !check_doublons(&all, &all.stA) || check_flag(av) < 0)
+			ft_error("full", &all);
+		all.stB.len = 0;
+		if (is_sorted(&all) < 1)
+			algo_check(&all);
+		free(all.stA.st);
+		free(all.stB.st);
+		all.count_rrb = ft_clear2(&all);
+		write(all.fd, all.str, all.count_rrb);
+		free(all.str);
+		if (all.flag[4])
+			freechar(all.av_dup);
+	}
+	return (0);
+}
