1df61eeecaf40a8ae7237b90a16b0e2517413ba6
JulesPelletier <julespelletier.perso@gmail.com>
2021-08-10 09:21:19 +0200

    2nd push pipex

--- Pipex/includes/pipex.h
+++ Pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: Jules <Jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 09:46:47 by Jules             #+#    #+#             */
-/*   Updated: 2021/07/29 17:31:21 by Jules            ###   ########.fr       */
+/*   Updated: 2021/08/09 18:18:22 by Jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,9 @@ typedef struct  s_pipex
     char    *command2;
 }               t_pipex;
 
+int     ft_get_pos(char *str, char c);
+char    *ft_strndup(char *str, unsigned int n);
+
 int     ft_open(char *filename, int n);
+
 #endif 
--- Pipex/srcs/pipex.c
+++ Pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: Jules <Jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 09:54:40 by Jules             #+#    #+#             */
-/*   Updated: 2021/08/09 11:58:21 by Jules            ###   ########.fr       */
+/*   Updated: 2021/08/09 18:21:59 by Jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int ft_open(char *filename, int n)
         if (access(filename, F_OK))
         {
             write(STDERR, "No such file\n", 14);
-            return (0);
+            return (STDIN);
         }
         return (open(filename, O_RDONLY));
     }
@@ -28,14 +28,49 @@ int ft_open(char *filename, int n)
             S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 }
 
+char    *ft_get_path(char *command1, char **env)
+{
+    char    *path;
+    char    *dir;
+    char    *tmp;
+    int     i;
+
+    i = 0;
+    while (env[i] && ft_strncmp(env[i], "PATH=", 5))
+        i++;
+    if (!env[i])
+        return (command1);
+    path = env[i] + 5;
+    while (path && ft_get_pos(path, ':') > -1)
+    {
+        dir = ft_strndup(path, ft_get_pos(path, ':'));
+        tmp = ft_strjoin(dir, command1);
+        free(dir);
+        if (access(tmp, F_OK) == 0)
+            return (tmp);
+        free(tmp);
+        path += ft_get_pos(path, ':') + 1;
+    }
+    return (command1);
+}
+
 void    execute_command(char *command, char **env)
 {
     char    **arg;
     char    *path;
+    int     i;
 
-    arg = env;
-    path = command;
-
+    i = 0;
+    (void)path;
+    (void)env;
+    arg = ft_split(command, ' ');
+    if (ft_get_pos(arg[0], '/') > -1)
+        path = arg[0];
+    else
+        path = ft_get_path(arg[0], env);
+    execve(path, arg, env);
+    write(STDERR, "The command was not found\n", 27);
+    exit(127);
 }
 
 void    redirect(char *command, char **env, int fd_in)
@@ -67,35 +102,38 @@ void    redirect(char *command, char **env, int fd_in)
     }
 }
 
-int main(int ac, char **av)
+void    test_aff(t_pipex pipex)
+{
+        printf("Infile : %s\n", pipex.infile);
+        printf("Outfile : %s\n", pipex.outfile);
+        printf("Fd IN : %d\n", pipex.fd_in);
+        printf("Fd OUT : %d\n", pipex.fd_out);
+        printf("Command 1 : %s\n", pipex.command1);
+        printf("Command 2 : %s\n", pipex.command2);
+}
+
+int main(int ac, char **av, char **env)
 {
     t_pipex pipex;
     
     if (ac == 5)
     {
         pipex.infile = av[1];
-        pipex.fd_in = ft_open(pipex.infile, 1);
+        pipex.fd_in = ft_open(pipex.infile, STDIN);
         pipex.outfile = av[ac - 1];
-        printf("Infile : %s\n", pipex.infile);
-        pipex.fd_out = ft_open(pipex.outfile, 2);
-        printf("Outfile : %s\n", pipex.outfile);
+        pipex.fd_out = ft_open(pipex.outfile, STDOUT);
         dup2(pipex.fd_in, STDIN);
-        printf("Fd IN : %d\n", pipex.fd_in);
-        printf("Fd OUT : %d\n", pipex.fd_out);
-        //dup2(pipex.fd_out, STDOUT);
-        printf("Fd OUT : %d\n", pipex.fd_out);
+        dup2(pipex.fd_out, STDOUT);
         pipex.command1 = av[2];
         pipex.command2 = av[3];
-        printf("Command 1 : %s\n", pipex.command1);
-        printf("Command 2 : %s\n", pipex.command2);
+        test_aff(pipex);
+        redirect(pipex.command1, env, pipex.fd_in);
+        execute_command(pipex.command2, env);
     }
-    /*
     else
     {
-        printf("%s\n", strerror(1));
-        printf("%s\n", strerror(2));
-        printf("%s\n", strerror(3));
-        printf("%s\n", strerror(4));
+        write(STDERR, "Number of arguments incorrect\n", 31);
+        write(STDERR, "([file1][command1][command2][file2])\n", 38);
     }
-    */
+    return (0);
 }
