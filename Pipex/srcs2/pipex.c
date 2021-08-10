928021103771aa72d36b7a6571a4da2a8dc5e5da
Jules PELLETIER <julpelle@e1r3p14.42.fr>
2021-08-10 17:00:04 +0200

    5th push pipex

--- Pipex/srcs2/pipex.c
+++ Pipex/srcs2/pipex.c
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 11:42:57 by julpelle          #+#    #+#             */
-/*   Updated: 2021/08/10 15:33:57 by julpelle         ###   ########.fr       */
+/*   Updated: 2021/08/10 16:59:03 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,27 @@ void    ft_path(char *command, char **args, char **paths, char **env)
     char    *tmp;
     char    *path;
 
+    int j=0;
+    ft_putstr("Affichage ARGS");
+    while (args[j])
+    {
+        ft_putstr(args[j]);
+        j++;
+    }
+    ft_putstr("\n");
+
     i = 0;
     while (paths[i])
     {
-        ft_putstr("CHECK\n");
         tmp = ft_strjoin(paths[i], "/");
-        ft_putstr(tmp);
         path = ft_strjoin(tmp, command);
+        ft_putstr("CHECK before execve\n\n");
+        ft_putstr(tmp);
+        ft_putstr("\n");
         ft_putstr(path);
+        ft_putstr("\n");
         execve(path, args, env);
+        ft_putstr("END execve");
         free(path);
         free(tmp);
         i++;
@@ -54,8 +66,15 @@ void    execute(char *command, char **args, char **env)
         {
             env[i] = env[i] + 5;
             paths = ft_split(env[i], ':');
-            write(0, paths[i], ft_strlen(paths[i]));
-            write(0, "\n", 1);
+            /*
+            int j=0;
+            while (paths[j])
+            {
+                ft_putstr(paths[j]);
+                ft_putstr("\n");
+                j++;
+            }
+            */
             break;
         }
         i++;
@@ -70,9 +89,9 @@ void    exec_cmd1(int pipefd[2], int fd_infile, char *command1, char **env)
     close(pipefd[0]);
     args = ft_split(command1, ' ');
     if (dup2(fd_infile, 0) == -1)
-        ft_exit("CH 1 : ERROR : failed to dup input with STDIN");
+        ft_exit("ERROR : failed to dup input with STDIN");
     if (dup2(pipefd[1], 1) == -1)
-        ft_exit("CH 2 : ERROR : failed to dup outfile with STDOUT");
+        ft_exit("ERROR : failed to dup outfile with STDOUT");
     if (args[0][0] == '/')
     {
         if (execve(args[0], args, env) == -1)
@@ -82,7 +101,6 @@ void    exec_cmd1(int pipefd[2], int fd_infile, char *command1, char **env)
         execute(args[0], args, env);
     close(fd_infile);
     close(pipefd[1]);
-    wait(NULL);
 }
 
 void    exec_cmd2(int pipefd[2], int fd_outfile, char *command2, char **env)
@@ -102,7 +120,6 @@ void    exec_cmd2(int pipefd[2], int fd_outfile, char *command2, char **env)
         execute(args[0], args, env);
     close(fd_outfile);
     close(pipefd[0]);
-    wait(NULL);
 }
 
 void    ft_pipex(int ac, char **av, char **env)
@@ -122,9 +139,15 @@ void    ft_pipex(int ac, char **av, char **env)
     if (pid < 0)
         ft_exit("ERROR : Failed to fork");
     if (pid == 0)
+    {
+        ft_putstr("CHECK COMMAND 2\n");
         exec_cmd2(pipefd, fd_outfile, av[3], env);
+    }
     else
+    {
+        ft_putstr("CHECK COMMAND 1\n");
         exec_cmd1(pipefd, fd_infile, av[2], env);
+    }
 }
 
 int main(int ac, char **av, char **env)
