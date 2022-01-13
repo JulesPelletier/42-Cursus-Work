c2b2ecf1aa33292673b447ef38a1a6a65f198162
Jules PELLETIER <julpelle@e2r10p1.clusters.42paris.fr>
2022-01-13 15:36:20 +0100

    CPP00 before push

--- CPP/CPP00/megaphone.cpp
+++ CPP/CPP00/megaphone.cpp
@@ -6,11 +6,34 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 18:26:38 by julpelle          #+#    #+#             */
-/*   Updated: 2022/01/11 18:26:48 by julpelle         ###   ########.fr       */
+/*   Updated: 2022/01/13 15:35:22 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
+#include <iostream>
+
 int	main(int ac, char **av)
 {
-	return (0);
+	int	i;
+	int	j;
+	
+	if (ac == 1)
+		std::cout << "* LOUD UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				av[i][j] = toupper(av[i][j]);
+				j++;
+			}
+			std::cout << av[i];
+			i++;
+		}
+		std::cout << std::endl;
+	}
 }
