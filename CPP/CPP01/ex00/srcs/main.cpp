628a07d1d3c1fd9da666512a4fdb9d31dccb2aee
Jules Pelletier <julespelletier.perso@gmail.com>
2022-01-17 23:02:55 +0100

    ex00 done

--- CPP/CPP01/ex00/srcs/main.cpp
+++ CPP/CPP01/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 22:12:07 by julpelle          #+#    #+#             */
-/*   Updated: 2022/01/17 22:54:35 by julpelle         ###   ########.fr       */
+/*   Updated: 2022/01/17 23:01:06 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,11 @@ int	main(int ac, char **av)
 	zombie4 = newZombie("Curtis");
 	zombie5 = newZombie("Mao");
 	zombie6 = newZombie("David");
+
+	randomChump("Marty");
+	
 	delete(zombie4);
 	delete(zombie5);
 	delete(zombie6);
-
-	randomChump("Marty");
 	return (0);
 }
