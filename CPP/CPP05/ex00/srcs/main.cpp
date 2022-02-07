b5a9c3b29ce129aff2a78661595e5db385e148d1
Jules Pelletier <julespelletier.perso@gmail.com>
2022-02-07 17:49:06 +0100

    new main

--- CPP/CPP05/ex00/srcs/main.cpp
+++ CPP/CPP05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 16:27:14 by julpelle          #+#    #+#             */
-/*   Updated: 2022/02/07 17:06:35 by julpelle         ###   ########.fr       */
+/*   Updated: 2022/02/07 17:43:02 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,74 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 
-	Bureaucrat	guest;
-	Bureaucrat	me("Jules", 120);
-	Bureaucrat	error1("Error Min", 149);
-	Bureaucrat	error2("Error Max", 1);
+	try
+	{
+		Bureaucrat	guest;
 
-	std::cout << Blue "Name > " Reset << guest.getName() << std::endl;
-	std::cout << Blue "Grade > " Reset << guest.getGrade() << std::endl;
+		std::cout << Blue "Name > " Reset << guest.getName() << std::endl;
+		std::cout << Blue "Grade > " Reset << guest.getGrade() << std::endl;
 
-	std::cout << Blue "Name > " Reset << me.getName() << std::endl;
-	std::cout << Blue "Grade > " Reset << me.getGrade() << std::endl;
+		guest.bettergrade();
+		guest.bettergrade();
+		guest.worsegrade();
 
-	std::cout << Blue "Name > " Reset << error1.getName() << std::endl;
-	std::cout << Blue "Grade > " Reset << error1.getGrade() << std::endl;
+		std::cout << guest << std::endl;
 
-	std::cout << Blue "Name > " Reset << error2.getName() << std::endl;
-	std::cout << Blue "Grade > " Reset << error2.getGrade() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
 
-	me.bettergrade();
-	me.bettergrade();
-	me.bettergrade();
-	guest.bettergrade();
-	guest.bettergrade();
-	guest.worsegrade();
-	error1.worsegrade();
-	//error2.bettergrade();
-	//error.worsegrade();
+	try
+	{
+		Bureaucrat	me("Jules", 120);
+		
+		std::cout << Blue "Name > " Reset << me.getName() << std::endl;
+		std::cout << Blue "Grade > " Reset << me.getGrade() << std::endl;
+		
+		me.bettergrade();
+		me.bettergrade();
+		me.bettergrade();
 
+		std::cout << me << std::endl;
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
 	
-	std::cout << std::endl;
-	std::cout << " ====== Final Results ====== " << std::endl;
-	std::cout << std::endl;
-	std::cout << me << std::endl;
-	std::cout << guest << std::endl;
-	std::cout << error1 << std::endl;
-	std::cout << error2 << std::endl;
+	try
+	{
+		Bureaucrat	error1("Error Min", 149);
+
+		std::cout << Blue "Name > " Reset << error1.getName() << std::endl;
+		std::cout << Blue "Grade > " Reset << error1.getGrade() << std::endl;
+		
+		error1.worsegrade();
+		error1.worsegrade();
+
+		std::cout << error1 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat	error2("Error Max", 1);
+
+		std::cout << Blue "Name > " Reset << error2.getName() << std::endl;
+		std::cout << Blue "Grade > " Reset << error2.getGrade() << std::endl;
+	
+		error2.bettergrade();
+		
+		std::cout << error2 << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+	
 }
