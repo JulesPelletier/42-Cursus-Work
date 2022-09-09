435af83f1c248041c62d5fab2aeb960de7322422
Jules PELLETIER <julpelle@e3r12p1.clusters.42paris.fr>
2022-09-09 16:46:16 +0200

    test

---
+++ inception_2/srcs/requirements/mariadb/script.sh
@@ -0,0 +1,24 @@
+chmod o+w /dev/stdout
+chmod o+w /dev/stderr
+if [ ! -d /var/lib/mysql/${DB_NAME} ]; then
+	echo "First run"
+	mysqld&
+	until mysqladmin ping; do
+		sleep 2
+	done
+    mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+    mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
+    mysql -u root -e "CREATE USER '${DB_ROOT}'@'%' IDENTIFIED BY '${DB_ROOTPSWRD}';"
+    mysql -u root -e "GRANT ALL ON *.* TO '${DB_ROOT}'@'%';"
+    mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USERPSWRD}';"
+    mysql -u root -e "GRANT ALL ON db_wordpress.* TO '${DB_USER}'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+	mysql -e "DELETE FROM mysql.user WHERE user=''"
+	mysql -e "DELETE FROM mysql.user WHERE user='root'"
+	mysql -e "FLUSH PRIVILEGES"
+	killall mysqld
+	echo "End first run"
+	else
+		echo "database already created"
+fi
+mysqld --slow-query-log-file=/dev/stderr --slow-query-log
