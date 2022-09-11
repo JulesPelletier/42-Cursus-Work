d85c41122fc62bf84a57afb31aba61a6a7301ef7
Jules PELLETIER <julpelle@e1r10p23.clusters.42paris.fr>
2022-09-11 13:29:41 +0200

    test 4

--- incep_final/my/srcs/requirements/mariadb/script.sh
+++ incep_final/my/srcs/requirements/mariadb/script.sh
@@ -8,9 +8,10 @@ if [ ! -d /var/lib/mysql/${DB_NAME} ]; then
 	done
     mysql -u root -e "CREATE DATABASE ${DB_NAME};"
     mysql -u root -e "SET GLOBAL general_log_file='mariadb.log';"
-    mysql -u root -e "CREATE USER '${DB_ROOT}'@'%' IDENTIFIED BY '${DB_ROOTPSWRD}';"
+    mysql -u root -e "CREATE USER '${DB_ROOT}'@'%' IDENTIFIED BY '${DB_ROOT_PWD}';"
+    mysql -u root -e "ALTER USER '${DB_ROOT}'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
     mysql -u root -e "GRANT ALL ON *.* TO '${DB_ROOT}'@'%';"
-    mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USERPSWRD}';"
+    mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"
     mysql -u root -e "GRANT ALL ON db_wordpress.* TO '${DB_USER}'@'%';"
     mysql -u root -e "FLUSH PRIVILEGES;"
 	mysql -e "DELETE FROM mysql.user WHERE user=''"
